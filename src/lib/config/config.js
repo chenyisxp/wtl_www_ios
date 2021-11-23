ec0da865d58b947386abde37bbf9b9cc95ff4bd0
chenyisxp <chenyisxp@163.com>
2021-11-23 20:08:41 +0800

    提交

--- src/lib/config/config.js
+++ src/lib/config/config.js
@@ -12,7 +12,7 @@ let BASE_CONFIG = {
     ROOT_URL:ROOT_URL,
     // 主服务地址
     SERVICE_ADDRESS : ROOT_URL + '/front/mainactivity',
-    liulanqiConnect:true,//浏览器调试的时候 !!!!!!!!!!!
+    liulanqiConnect:false,//浏览器调试的时候 !!!!!!!!!!!
     ONLY_CONNECT_STATUS_TOAST:false,
     DEVELOPERMODEFLAG:false,//开发者模式本地完全不和安卓交互
     LOGFLAG:false,
@@ -20,7 +20,7 @@ let BASE_CONFIG = {
     TESTDEEPTH:0,//深度
     autoRouterTime:5000,//自动前往焊接中页面的时间：ms
     scaningDuring:15000,//扫描时间
-    ENV_IOS_FLAG : true,//true:ios、fasle:安卓环境 !!!!!!!!!!!!!!!!!!!!!!!!!!
+    ENV_IOS_FLAG : false,//true:ios、fasle:安卓环境 !!!!!!!!!!!!!!!!!!!!!!!!!!
     DirectStart:'DA',
     callWeldTypeData:{//注意utils中有独立crccode注意一起改
        migsyn:{data:'100000',crcCode:'0570',name:'MIG SYN',modelKey:'MIGSYN',url:'/weld_common?type=MIGSYN',newIndex:0},
